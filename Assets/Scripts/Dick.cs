ceb8088f8894faf8d6e36ac03a3be34aba5e1356
Aaron Martinez <unluckyflamingo98@gmail.com>
2020-10-08 17:13:51 +0200

    Animations

--- Assets/Scripts/Dick.cs
+++ Assets/Scripts/Dick.cs
@@ -9,11 +9,13 @@ public class Dick : MonoBehaviour
 
     private bool isDead = false;
     private Rigidbody2D get2D;
+    private Animator getAnim;
 
     // Asignamos el componente rigidbody2D a la variable "get2d"
     void Start()
     {
         get2D = GetComponent<Rigidbody2D>();
+        getAnim = GetComponent<Animator>();
     }
 
     // Se comprueba si el jugador está muerto a cada frame para aplicar las mecánicas de juego. 
@@ -25,6 +27,7 @@ public class Dick : MonoBehaviour
             {
                 get2D.velocity = Vector2.zero; // Se iguala la velocidad a 0 para que siempre se obtenga el mismo valor en el salto 
                 get2D.AddForce(new Vector2(0, jump)); // Se aplica la fuerza de la variable "jump" al vector 2, se hace una variable publica para que edite desde el propio unity
+                getAnim.SetTrigger("Flap"); // Se activa el trigger "Flap" para la animación
             }
         }
     }
@@ -33,6 +36,7 @@ public class Dick : MonoBehaviour
     private void OnCollisionEnter2D()
     {
         isDead = true; // Cambiamos la variable a true para que no podamos seguir dandole al ratón.
+        getAnim.SetTrigger("Die"); //Se activa el trigger "Die" para la animación
     }
 
 }
